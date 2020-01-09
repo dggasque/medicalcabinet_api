051485675dbd5810523b03ac5b4f4cadc2b5b63b
dggasque <dggasque@users.noreply.guthub.com>
2020-01-08 16:10:51 -0800

    adds general search api

--- app.py
+++ app.py
@@ -53,6 +53,28 @@ def symptom():
     # give output to sender.
     return output
 
+@app.route('/general', methods=['Post'])
+def general():
+    """ a route, expects json object with 1 key """
+
+    # receive input
+    lines = request.get_json(force=True)
+
+    # get data from json
+    text = lines['input']  # json keys to be determined
+
+    # validate input (optional)
+    assert isinstance(text, str)
+
+
+    # predict
+    output = predict_all(text)
+
+
+    # give output to sender.
+    return output
+
+
 # 4 spaced symptoms json version
 # user input
 user_input_symp = "multiple sclerosis, epilepsy, pain, "
@@ -60,10 +82,10 @@ user_input_symp = "multiple sclerosis, epilepsy, pain, "
 def predict_symptoms(user_input_symp):
 
     #unpickling file of embedded cultivar symptoms diseases
-    unpickled_df_test = pd.read_pickle("./symptommedembedv6.pkl")
+    unpickled_df_test = pd.read_pickle("./symptommedembedv8.pkl")
 
     # getting data
-    df = pd.read_csv('symptoms6_medcab3.csv')
+    df = pd.read_csv('symptoms8_medcab3.csv')
 
     # Part 1
     # a function to calculate_user_text_embedding
@@ -131,7 +153,7 @@ user_input = "text, Relaxed, Violet, Aroused, Creative, Happy, Energetic, Flower
 def predict(user_input):
 
     # getting data
-    df = pd.read_csv('symptoms6_medcab3.csv')
+    df = pd.read_csv('med1.csv')
 
     #effcts unpickling file of embedded cultivar descriptions
     unpickled_df_test = pd.read_pickle("./medembedv2.pkl")
@@ -182,6 +204,74 @@ def predict(user_input):
         
         #stores the score in the dataframe
         df.loc[i,'score'] = score
+
+    # Part 4: returns all data for the top 5 results as a json obj
+    df_big_json = df.sort_values(by='score', ascending=False)
+    df_big_json = df_big_json[:5]
+    df_big_json = df_big_json.to_json(orient='columns')
+
+    # Part 5: output
+    return df_big_json
+
+# user input
+user_input = "multiple sclerosis, epilepsy, pain, "
+
+
+def predict_all(user_input_all):
+
+    #unpickling file of embedded cultivar symptoms diseases
+    unpickled_df_test = pd.read_pickle("./all_text_medembedv8.pkl")
+
+    # getting data
+    df = pd.read_csv('symptoms8_medcab3.csv')
+
+    # Part 1
+    # a function to calculate_user_text_embedding
+    # to save the embedding value in session memory
+    user_input_embedding = 0
+
+    def calculate_user_text_embedding(input, user_input_embedding):
+
+        # setting a string of two sentences for the algo to compare
+        sentences = [input]
+
+        # calculating embedding for both user_entered_text and for features
+        with basilica.Connection('36a370e3-becb-99f5-93a0-a92344e78eab') as c:
+            user_input_embedding = list(c.embed_sentences(sentences))
+        
+        return user_input_embedding
+
+    # run the function to save the embedding value in session memory
+    user_input_embedding = calculate_user_text_embedding(user_input_all, user_input_embedding)
+
+
+
+
+    # part 2
+    score = 0
+
+    def score_user_input_from_stored_embedding_from_stored_values(input, score, row1, user_input_embedding):
+
+        # obtains pre-calculated values from a pickled dataframe of arrays
+        embedding_stored = unpickled_df_test.loc[row1, 0]
+        
+        # calculates the similarity of user_text vs. product description
+        score = 1 - spatial.distance.cosine(embedding_stored, user_input_embedding)
+
+        # returns a variable that can be used outside of the function
+        return score
+
+
+
+    # Part 3
+    for i in range(2351):
+        # calls the function to set the value of 'score'
+        # which is the score of the user input
+        score = score_user_input_from_stored_embedding_from_stored_values(user_input, score, i, user_input_embedding)
+        
+        #stores the score in the dataframe
+        df.loc[i,'score'] = score
+
 
     # Part 4: returns all data for the top 5 results as a json obj
     df_big_json = df.sort_values(by='score', ascending=False)
